4fcbcf17a9bef524645a4dcf21e0778772053624
vika_taran <victoriataran.mail@gmail.com>
2023-01-10 12:32:22 +0200

    Added container to Root Component

--- src/views/Root/index.tsx
+++ src/views/Root/index.tsx
@@ -1,17 +1,20 @@
 import React from "react";
-import { Container } from "@mui/material";
-
+import Container from "@mui/material/Container";
 import { Header } from "./components/Header";
 import { Main } from "./components/Main";
-import { AuthSignIn, AuthSignUp } from "../auth";
+import CssBaseline from "@mui/material/CssBaseline";
+import Box from "@mui/material/Box";
 
 export function Root() {
   return (
-    <Container>
-      <Header />
-      <Main />
-      {/* <AuthSignIn />
-      <AuthSignUp /> */}
-    </Container>
+    <React.Fragment>
+      <CssBaseline />
+      <Container maxWidth="lg">
+        <Box sx={{ height: "100vh" }}>
+          <Header />
+          <Main />
+        </Box>
+      </Container>
+    </React.Fragment>
   );
 }
