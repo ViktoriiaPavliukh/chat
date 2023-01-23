2d8e8c06bd8c082037567b55f435722c07d9225c
ViktoriiaPavliukh <victoriataran.mail@gmail.com>
2023-01-23 16:53:28 +0000

    Added redux logger

--- src/core/store/index.ts
+++ src/core/store/index.ts
@@ -1,10 +1,18 @@
 import { applyMiddleware, compose, createStore } from "redux";
+import thunk from "redux-thunk";
+import { createLogger } from "redux-logger";
 
 import rootReducer from "./rootReducer";
 
 import messagesMiddleware from "./messages/middleware";
+// import usersMiddleware from './users/middleware';
 
-const middlewares = [messagesMiddleware];
+const middlewares = [
+  thunk,
+  createLogger({ collapsed: true }),
+  messagesMiddleware,
+  // usersMiddleware,
+];
 
 export default createStore(
   rootReducer,
